c4b76d426c899b620fc13f9491828f76edcc221b
AllisonWonderland <aneary91@gmail.com>
2021-04-06 18:11:30 -0500

    added everything and forgot to commit

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
-var timerEl = document.querySelector(".timer")
-var mainEl = document.getElementById("main")
+var timerEl = document.querySelector(".timer");
+var startBtn = document.getElementById("#startBtn");
+var secondsLeft = 10;
+
+function setTimer(){
+
+
+    timerInterval = setInterval(funtion(){
+        secondsLeft--;
+        timerEl.textContent = countDown + "secondsLeft"
+            if (countDown === 0) {
+                
+
+            }
+
+    },1000)
+}
+
+    startBtn.addEventListener("click", function(event){
+        clearInterval(timerInterval);
+        countDown - 10;
+        setTimer();
+
+
+})
