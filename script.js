8778cb53de776e414917c8aa90aae07e60624ed7
AllisonWonderland <aneary91@gmail.com>
2021-04-08 12:10:17 -0500

    getting help

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ var secondsLeft = 10;
 
 // variables to grab DOM elements
 
-var questionNumEl = document.getElementById("questionNumber")
-var questionEl = document.getElementById(quizQuestionEl);
+var questionNumEl = document.getElementById("questionNumber");
+var questionEl = document.getElementById("quizQuestionEl");
 var questionElOneEl = document.getElementById("question1");
 var questionElTwoEl = document.getElementById("question2");
 var questionElThreeEl = document.getElementById("question3");
@@ -18,67 +18,61 @@ var questionIndex = 0; 1; 2; 3;
 
 // variable of objects and 
 var jsQuestions = [
-    {
-        
-        
-   {
-        question: "How do you write an If Statement in JavaScript?"
-        optionOne: "if i=5 then",
-        optionTwo: "if i = 5",
-        optionThree: "if i == 5 then",
-        optionFour: "if (i=5) {}";
-        answer: "if (i=5) {}"
- }
- {
-            question: "How do you create a function in JavaScipt?",
-            optionOne: "function myFunction()",
-            optionTwo: "function=myFunction()",
-            optionThree: "fucntion:myFunction()",
-            answer: ["function myFunction() {}"]
- }
- {
-            question: "Inside which HTML element do we put the JavaScript?",
-            optionOne: "js Tag",
-            optionTwo: "javaScriptTag",
-            optionThree: "scriptTag",
-            optionFour: "scripting tag"
-         }
+        {
+         question: "How do you write an If Statement in JavaScript?",
+         optionOne: "if i=5 then",
+         optionTwo: "if i = 5",
+         optionThree: "if i == 5 then",
+         optionFour: "if (i=5) {}",
+         answer: "if (i=5) {}"
+        },
+       {
+         question: "How do you create a function in JavaScipt?",
+         optionOne: "function myFunction()",
+         optionTwo: "function=myFunction()",
+         optionThree: "fucntion:myFunction()",
+         answer: ["function myFunction() {}"]
+       }, 
+        {
+         question: "Inside which HTML element do we put the JavaScript?",
+         optionOne: "js Tag",
+         optionTwo: "javaScriptTag",
+         optionThree: "<script> Tag",
+         optionFour: "scripting tag",
+         answer: "<script> Tag"
+        },
 
         {
-            question: "The external JavaScript file must contain the script tag?"
-            optionOne: true,
-            optionTwo: false,
-
+         question: "The external JavaScript file must contain the script tag?",
+         optionOne: true,
+         optionTwo: false,
+         answer: ""
           }
 
 ]
 
-console.log(choices.qNum1);
-
-
 function setTimer(){
 
 
-    timerInterval = setInterval(funtion(){
+    timerInterval = setInterval(function() {
         secondsLeft--;
-        timerEl.textContent = countDown + "secondsLeft"
-            if (countDown === 0) {
+        timerEl.textContent = countDown + "secondsLeft" 
+            if (countDown === 10) {
                 
 
             }
 
-    },1000)
+    }, 1000)
 }
 
 function startQuiz() {
-    startBtn.addEventListener("click", function(event){
+    startBtn.addEventListener("click", function() {
          clearInterval(timerInterval);
             countDown - 10;
             setTimer();
-
+    
+            
             // hide the quizStartElcontainer after the start button is pressed
-            quizStartEl.setAttribute("class", "hidden");
-}
-
-
-})
+            quizStartEl.setAttribute("class", "hidden")
+    }, 1000)
+};
