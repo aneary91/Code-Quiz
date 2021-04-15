1f4f01d5f9695afd4e27ecd38cd014f2f96abc5f
AllisonWonderland <aneary91@gmail.com>
2021-04-14 21:30:31 -0500

    sending changes

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var timerEl = document.querySelector(".timer");
 var startBtn = document.getElementById("#startBtn");
-var secondsLeft = 10;
+var secondsLeft = 60;
 
 
 // variables to grab DOM elements
@@ -16,9 +16,15 @@ var quizStartEl = document.querySelector(".codeQuizStart");
 
 var questionIndex = 0; 1; 2; 3;
 
+// 1. Create a loop that loops over your array
+// 2. Try to console log the items in your array one at a time
+// 3. Then try to console log the question
+// 4. Then see if you can display that question
+
+
 // variable of objects and 
 var jsQuestions = [
-        {
+    {
          question: "How do you write an If Statement in JavaScript?",
          optionOne: "if i=5 then",
          optionTwo: "if i = 5",
@@ -26,44 +32,56 @@ var jsQuestions = [
          optionFour: "if (i=5) {}",
          answer: "if (i=5) {}"
         },
-       {
-         question: "How do you create a function in JavaScipt?",
-         optionOne: "function myFunction()",
+        {
+            question: "How do you create a function in JavaScipt?",
+            optionOne: "function myFunction()",
          optionTwo: "function=myFunction()",
          optionThree: "fucntion:myFunction()",
          answer: ["function myFunction() {}"]
-       }, 
+        }, 
         {
-         question: "Inside which HTML element do we put the JavaScript?",
-         optionOne: "js Tag",
+            question: "Inside which HTML element do we put the JavaScript?",
+            optionOne: "js Tag",
          optionTwo: "javaScriptTag",
          optionThree: "<script> Tag",
          optionFour: "scripting tag",
          answer: "<script> Tag"
         },
-
+        
         {
-         question: "The external JavaScript file must contain the script tag?",
-         optionOne: true,
-         optionTwo: false,
-         answer: ""
-          }
+            question: "The external JavaScript file must contain the script tag?",
+            optionOne: true,
+            optionTwo: false,
+            answer: ""
+        }
+        
+    ]
 
-]
+    function showQuestions() {
+        var  myQuestionHTML = "";
 
-function setTimer(){
+        for (i = 0; i < jsQuestions.length; i++) {
+            myQuestionHTML+='<div class="questionsContainer hidden"><h2 id="quizQuestion'+i+'">Quiz Question #'+(i+1)+'</h2><p>'+jsQuestions[i].question+'</p> <button>'+jsQuestions[i].optionOne+'</button>';
 
+        }
+           document.getElementById("allQuestions").innerHTML = myQuestionHTML; 
+     }
+        showQuestions();
 
+    
+    function setTimer(){
+ 
     timerInterval = setInterval(function() {
         secondsLeft--;
-        timerEl.textContent = countDown + "secondsLeft" 
-            if (countDown === 10) {
-                
-
+        timerEl.textContent = secondsLeft + " seconds left!"; 
+            if (secondsLeft === 10) {
+                alert("hurry up!");
             }
 
-    }, 1000)
+    }, 1000);
 }
+setTimer();
+
 
 function startQuiz() {
     startBtn.addEventListener("click", function() {
@@ -73,6 +91,6 @@ function startQuiz() {
     
             
             // hide the quizStartElcontainer after the start button is pressed
-            quizStartEl.setAttribute("class", "hidden")
-    }, 1000)
-};
+            quizStartEl.setAttribute("class", "hidden");
+    }, 1000);
+}
