f31d61ba9e05cd5aaa9257b2a1ccdb017ccd77a8
AllisonWonderland <aneary91@gmail.com>
2021-06-17 11:49:48 -0500

    question is now appendign to the page and wrote a check answer funciton

--- script.js
+++ script.js
@@ -2,144 +2,116 @@ var startBtn = document.getElementById("startBtn");
 var cue = document.querySelector("#cue");
 startBtn.addEventListener("click", startQuiz);
 
-
 var score = 0;
 var penalty = 10;
 
-var interval ;
+var interval;
 var secondsLeft = 60;
 var timerEl = document.getElementById("countdown");
 
-var question = document.querySelector("#questions");
-var question1OneEl = document.querySelector("#question1");
-var question2TwoEl = document.querySelector("#question2");
-var question3ThreeEl = document.querySelector("#question3");
-var question4FourEl = document.querySelector("#question4");
-var qCont = document.getElementById('questionsContainer');
+var qCont = document.getElementById("questionsContainer");
 
-var gameScore = document.querySelector("#game-score")
+var gameScore = document.querySelector("#game-score");
+
+var optionsEl = document.getElementById("options");
 
 var totalScoreEl = document.querySelector("#total-score");
 
 var submitEl = document.querySelector("#submit");
 
-// this is the list of questions and answers 
+// this is the list of questions and answers
 var questionArray = [
-{
+  {
     question: "How do you write an If Statement in JavaScript?",
     choices: ["if i=5 then", "if i = 5", "if i == 5 then", "if (i=5) {}"],
     answer: "if (i=5) {}",
-},
-{
+  },
+  {
     question: "How do you create a function in JavaScipt?",
-    choices: ["function myFunction()", "function=myFunction()", "function myFunction() {}", "function:myFunction()"],
-    answer: "function myFunction() {}"
-},
-{
+    choices: [
+      "function myFunction()",
+      "function=myFunction()",
+      "function myFunction() {}",
+      "function:myFunction()",
+    ],
+    answer: "function myFunction() {}",
+  },
+  {
     question: "Inside which HTML element do we put the JavaScript?",
     choices: ["js Tag", "javaScriptTag", "<script> Tag", "scripting tag"],
     answer: "<script> Tag",
-},
-{
+  },
+  {
     question: "The external JavaScript file must contain the script tag?",
-    choices: ['true','false'],
-    answer: 'false',
-}
-]
-var myIndex = 0
+    choices: ["true", "false"],
+    answer: "false",
+  },
+];
+var myIndex = 0;
 
 // a function to start the quiz
 function startQuiz() {
-    interval = setInterval(setTimer, 1000)
-    var startCont = document.getElementById('startContainer');
-    startCont.setAttribute('class', 'hide')
+    interval = setInterval(setTimer, 1000);
+    var startCont = document.getElementById("startContainer");
+    startCont.setAttribute("class", "hide");
 
-    qCont.removeAttribute('class')
-    
+    qCont.removeAttribute("class");
+
+    showQuestions()
 }
 // a function to set the timer for the quiz
 function setTimer() {
     secondsLeft--;
     timerEl.textContent = secondsLeft;
 
-    if (secondsLeft <= 0 ){
-        endGame()
+    if (secondsLeft <= 0) {
+    endGame();
     }
 }
 
+//a function to show questions and options.. the questionArray displays the first question because my index is equal to zero
 function showQuestions() {
+    qCont.removeAttribute('class');
 
+  //dom element to display the question
+    var showQ = document.getElementById("allQuestions");
+    var presentQ = questionArray[myIndex];
+  //append the question to the DOM .. its going to show the text content from the value fro mteh question key
+    showQ.textContent = presentQ.question;
+  //this clears the old choices theat might be displaying on the page ..
+  optionsEl.innerHTML = "";
 
-        if (myIndex < questions.length){
-            question.textContent = questionArray[myIndex].question;
-            question1El.textContent = questionArray[myIndex].question1;
-            question2El.textContent = questionArray[myIndex].question2;
-            question3El.textcontent = questionArray[myIndex].question3;
-            question4El.textcontent = questionArray[myIndex].question4;
-            answer = questions[myIndex].answer;
-            
-            if (secondsLeft === 0) {
-                question1El.onclick = function () {
-                    if(questionArray[myIndex].questionOneEl == answer){
-                        gameScore++;
-                        myIndex++;
-                        showQuestions()
-                    } else {
-                        secondsLeft = secondsLeft - 10
-                    }
-                }
-                question2El.onclick = function () {
-                    if(questionArray[myIndex].questionTwoEl == answer){
-                        gameScore++;
-                        myIndex++;
-                        showQuestions()
-                    } else {
-                        secondsLeft = secondsLeft - 10
-                    }
-                }
-                question3El.onclick = function () {
-                    if(questionArray[myIndex].questionThreeEl == answer){
-                        gameScore++;
-                        myIndex++;
-                        showQuestions()
-                    } else {
-                        secondsLeft = secondsLeft - 10
-                    }
-                }
-                question4El.onclick = function () {
-                    if(questionArray[myIndex].questionFourEl == answer){
-                        gameScore++;
-                        myIndex++;
-                        showQuestions()
-                    } else {
-                        secondsLeft = secondsLeft - 10
-                    }
-                }
-            }
-        } else {
-            console.log("Congrats on Completing this Quiz!")
-            currentTime = 0
-            finalScore.append(score)
-            endGame()
-        }
+  presentQ.choices.forEach(function (index, choice) {
+    //create a button for each choice in the choices array
+    var optionBtn = document.createElement("button");
+    optionBtn.setAttribute("class", "choiceBtn");
+    optionBtn.setAttribute("value", choice);
+
+    optionBtn.textContent = index + 1 + ". " + choice;
+    optionBtn.onClick = checkAnswer();
+    //append the buttons to the HTML element
+    optionsEl.appendChild(optionBtn);
+    });
+}
+
+function checkAnswer() {
+    if (this.value !== questionsArray[myIndex].answer){
+        secondsLeft -=5
     }
-    function endGame() {
-        question.style.display = 'none';
-        option1El.style.display = 'none';
-        option2El.style.display = 'none';
-        option3El.style.display = 'none';
-        option4El.style.display = 'none';
+    //stops the timer from showing negative intergers
+    if (secondsLeft <0 ){
+        secondsLeft = 0;
     }
+    myIndex ++
+}
 
-    function saveScore() {
-        let lastName = document.getElementById('inputLastName').Value;
-        console.log(lastName)
-        let highScore = JSON.parse(localStorage.getItem('highScores'))
-    }
+function endGame() {
 
-
+}
 
 
-
-
-
+function saveScore() {
+    let lastName = document.getElementById("inputLastName").Value;
+    console.log(lastName);
+    let highScore = JSON.parse(localStorage.getItem("highScores"));
+}
